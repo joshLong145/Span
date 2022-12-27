1b9f4ba407990f63bd19291b5e4acf96d213fdc0
jlong145 <joshlong0795@gmail.com>
2022-12-27 17:04:47 -0500

    update to example for wasm webauthn testing

--- examples/wasm/example.ts
+++ examples/wasm/example.ts
@@ -6,69 +6,69 @@ class Example extends WasmWorkerDefinition {
         super(modulePath);
     }
 
-    public test2(buffer: SharedArrayBuffer, module: any) {
-        let arr = new Int8Array(buffer);
-        arr[0] += 1
-        //@ts-ignore
-        self.primeGenerator()
-        return arr.buffer
-    }
-    
-   public test1(buffer: SharedArrayBuffer, module: any) {
-        let arr = new Int32Array(buffer);
-        var myString = 'A rather long string of English text, an error message \
-                actually that just keeps going and going -- an error \
-                message to make the Energizer bunny blush (right through \
-                those Schwarzenegger shades)! Where was I? Oh yes, \
-                you\'ve got an error and all the extraneous whitespace is \
-                just gravy.  Have a nice day.'
-        for (var index = 0; index < 2048; index++) {
-            let hash = 0;
-            for (let i = 0, len = myString.length; i < len; i++) {
-                let chr = myString.charCodeAt(i);
-                hash = (hash << 5) - hash + chr;
-                hash |= 0; // Convert to 32bit integer
+    public init(buffer: SharedArrayBuffer, module: any) {
+        console.log("initalizing webauthn instance...")
+        const options = {
+            RPDisplayName: "localhost",
+            RPID: "locahost",
+            RPOrigin: "http://localhost:8080",
+            RPIcon: "",
+            AttestationPreference: "direct",
+            AuthenticatorSelection: {
+                AuthenticatorAttachment: "platform",
+                RequireResidentKey: true,
+                UserVerification: "required"
             }
-            arr[index] = hash
+        };
+
+        //@ts-ignore
+        self.WebAuthnGoJS.CreateContext(JSON.stringify(options), function(arg, msg){
+            console.log(arg);
+            console.log(msg);
+        });
+
+        return buffer
+    }
+
+    /*
+	ID          uint64                `json:"id"`
+	Name        string                `json:"name"`
+	DisplayName string                `json:"displayName"`
+	Credentials []webauthn.Credential `json:"credentials"`
+    */
+    public createRegistrationOptions(buffer: SharedArrayBuffer, module: any, args: any): Promise<any>  {
+        console.log("beginning registration")
+        
+        const user = {
+            id: 1,
+            name: args.name,
+            displayName: "foo",
+            credentials: []
         }
-        return arr.buffer;
+        let result;
+        //@ts-ignore
+        self.WebAuthnGoJS.BeginRegistration(JSON.stringify(user), (err, msg) => {
+            console.log(err);
+            console.log(msg);
+            result = msg;     
+        });
+
+
+        return result;
+    }
+
+    public finishRegistration(buffer: SharedArrayBuffer, module: any, args: any): void {
+        
+
     }
 }
 
-const example: WasmWorkerDefinition = new Example("./examples/wasm/primes-2.wasm");
+const example: WasmWorkerDefinition = new Example("./examples/wasm/main.wasm");
 
-const wrapper: WasmInstanceWrapper<Example> = new WasmInstanceWrapper<Example>(example, {
-    outputPath: 'output'
+const wrapper: WasmInstanceWrapper<Example> = new WasmInstanceWrapper<Example>(example as Example, {
+    outputPath: 'test-server/public'
 });
 
-wrapper.start();
-//@ts-ignore
-await example.execute("test1").then((buf: SharedArrayBuffer) => {
-    console.log("hello", new Int32Array(buf))
-})
-await example.execute("test2").then((buf: SharedArrayBuffer) => {
-    let arr = new Int32Array(buf);
-    console.log("hello1", new Int32Array(buf)[0])
-})
-
-await example.execute("test2").then((buf: SharedArrayBuffer) => {
-    console.log("hello2",  new Int32Array(buf)[0])
-})
-await example.execute("test2").then((buf: SharedArrayBuffer) => {
-    console.log("hello3",  new Int32Array(buf)[0])
+wrapper.Create({
+    writeFileSync: Deno.writeFileSync
 });
-
-example.terminateWorker()
-
-wrapper.restart()
-
-await example.execute("test1").then((buf: SharedArrayBuffer) => {
-    console.log("hello", new Int32Array(buf))
-})
-
-await example.execute("test2").then((buf: SharedArrayBuffer) => {
-    let arr = new Int32Array(buf);
-    console.log("hello1", new Int32Array(buf)[0])
-})
-
-example.terminateWorker();
