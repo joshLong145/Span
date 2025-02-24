643bf7432edfdace188755b45d06e3d971d1ae2c
Josh Long <jlong2@ramapo.edu>
2025-02-23 21:51:34 -0500

    ref: add option pass through

--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,11 @@
 import { InstanceWrapper, WorkerDefinition } from "./InstanceWrapper.ts";
+import { InstanceConfiguration } from "./types.ts";
 
 export async function wrap<T extends WorkerDefinition>(
   wrapper: T,
+  options: InstanceConfiguration | undefined
 ): Promise<InstanceWrapper<T>> {
-  const instance = new InstanceWrapper<T>(wrapper, {});
+  const instance = new InstanceWrapper<T>(wrapper, options ?? {});
   await instance.start().catch((e: unknown) => {
     console.log("failed to start instance: ", e as Error);
     throw e;
