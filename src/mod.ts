fdd67858db23f3db37fbdd5b30725e298b347081
Josh Long <jlong2@ramapo.edu>
2025-02-23 21:49:20 -0500

    feat: add wrap for InstanceWrapper wrapping

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,15 @@
 import { InstanceWrapper, WorkerDefinition } from "./InstanceWrapper.ts";
 
-export { InstanceWrapper, WorkerDefinition };
+export async function wrap<T extends WorkerDefinition>(
+  wrapper: T,
+): Promise<InstanceWrapper<T>> {
+  const instance = new InstanceWrapper<T>(wrapper, {});
+  await instance.start().catch((e: unknown) => {
+    console.log("failed to start instance: ", e as Error);
+    throw e;
+  });
+  return instance;
+}
 
+export { InstanceWrapper, WorkerDefinition };
 export * from "./types.ts";
